608ff0f734653585c907cc246ade23c5aa48e60d
khangnld <163306744+khangnld@users.noreply.github.com>
2025-06-18 16:54:32 +0700

    fix menu display and add countbuy for menuitem

--- Pages/Menu.cshtml.cs
+++ Pages/Menu.cshtml.cs
@@ -18,13 +18,26 @@ namespace PRN222_Restaurant.Pages
 
         public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
         public List<Category> Categories { get; set; } = new List<Category>();
+        public List<MenuItem> TopRecommendations { get; set; } = new List<MenuItem>();
+
 
         public async Task OnGet()
         {
             // Lấy danh sách danh mục và món ăn từ cơ sở dữ liệu
             Categories = await _context.Categories.ToListAsync();
+
+            // Lấy danh sách món ăn chỉ với Status = Available
             MenuItems = await _context.MenuItems
-                .Include(m => m.Category) // Bao gồm thông tin danh mục
+                .Where(m => m.Status == MenuItemStatus.Available)
+                .Include(m => m.Category)
+                .ToListAsync();
+
+            // Lấy 3 món bán chạy nhất (BuyCount cao hơn, Tiền cao hơn và Status còn hàng hay không)
+            TopRecommendations = await _context.MenuItems
+                .OrderByDescending(m => m.BuyCount)
+                .ThenByDescending(m => m.Price)
+                .ThenBy(m => m.Status)
+                .Take(3)
                 .ToListAsync();
         }
     }
