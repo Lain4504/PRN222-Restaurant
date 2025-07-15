fd794e9ce8b4eff558bf28d65e1f87dd6efc9e15
khangnld <163306744+khangnld@users.noreply.github.com>
2025-07-15 08:05:00 +0700

    update home list menu item

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using PRN222_Restaurant.Models;
 using PRN222_Restaurant.Services;
+using PRN222_Restaurant.Services.IService;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,15 +12,18 @@ namespace PRN222_Restaurant.Pages
     public class IndexModel : PageModel
     {
         private readonly IFeedbackService _feedbackService;
+        private readonly IMenuItemService _menuItemService;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(IFeedbackService feedbackService, ILogger<IndexModel> logger)
+        public IndexModel(IFeedbackService feedbackService, ILogger<IndexModel> logger, IMenuItemService menuItemService)
         {
             _feedbackService = feedbackService;
             _logger = logger;
+            _menuItemService = menuItemService;
         }
 
         public List<Feedback> Feedbacks { get; set; } = new();
+        public List<MenuItem> FeaturedDishes { get; set; } = new();
 
         public async Task OnGetAsync()
         {
@@ -28,8 +32,15 @@ namespace PRN222_Restaurant.Pages
             Feedbacks = allFeedbacks
                 .OrderByDescending(f => f.CreatedAt)
                 .ToList();
+
+            var allMenuItems = await _menuItemService.GetAllAsync();
+
+            FeaturedDishes = allMenuItems
+                .Where(m => m.Status == MenuItemStatus.Available)
+                .OrderByDescending(m => m.BuyCount)
+                .ThenByDescending(m => m.Price)
+                .Take(3) // Lấy top 3 món ăn nổi bật
+                .ToList();
         }
-
     }
-
 }
